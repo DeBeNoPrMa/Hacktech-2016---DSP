0142d398bb884cb0383b5d8a13d0b9653b737872
debenoprma <coll.marce@gmail.com>
2016-02-27 21:43:34 -0800

    Lots of things

--- src/SoundInput.java
+++ src/SoundInput.java
@@ -1,30 +1,54 @@
 import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.AudioEvent;
 import be.tarsos.dsp.io.jvm.JVMAudioInputStream;
 import be.tarsos.dsp.onsets.OnsetHandler;
 import be.tarsos.dsp.onsets.PercussionOnsetDetector;
+import be.tarsos.dsp.pitch.PitchDetectionHandler;
+import be.tarsos.dsp.pitch.PitchDetectionResult;
+import be.tarsos.dsp.pitch.PitchProcessor;
 import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
 import javax.sound.sampled.*;
 import javax.swing.*;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Marce Coll on 27/02/16.
  */
-public class SoundInput implements OnsetHandler {
+
+enum Note {
+    F, G, A, None
+}
+
+public interface PitchHandler {
+    public void insertIntoQueue();
+}
+
+public class SoundInput implements OnsetHandler, PitchDetectionHandler {
     // Audio helper classes
     Mixer soundMixer;
     BetterAudioDispatcher dispatcher;
+
+    PitchProcessor.PitchEstimationAlgorithm algo;
 
     // Parameters
     double sensitivity = 35;
     double threshold = 10;
 
-    final int sampleRate = 32768;
+    final int sampleRate = 44100;
     final int bufferSize = 1024;
     final int overlap = 0;
+
+    // NOTE PARAMETERS
+    float noteOffset = 0.0f;
+    Map<Note, Float> baseFreq = new EnumMap<Note, Float>(Note.class);
+    float bpm;
+
+    PitchHandler handler;
 
     public SoundInput() {
         // Setup the sound dispatcher
@@ -36,7 +60,7 @@ public class SoundInput implements OnsetHandler {
         }
     }
 
-    public void setupDispatcher()
+    public void setupDispatcher(float bpm, PitchHandler h)
             throws LineUnavailableException,
             UnsupportedAudioFileException
     {
@@ -45,6 +69,18 @@ public class SoundInput implements OnsetHandler {
         // Info about the sound data line
         final DataLine.Info dataLineInfo = new DataLine.Info(TargetDataLine.class, audioFormat);
 
+        // Pitch Recognition algorithm
+        algo = PitchProcessor.PitchEstimationAlgorithm.DYNAMIC_WAVELET;
+
+        // Base frequencies
+        baseFreq.put(Note.F, 349.23f);
+        baseFreq.put(Note.G, 392.00f);
+        baseFreq.put(Note.A, 440.00f);
+
+        handler = h;
+        this.bpm = bpm;
+
+        // Setup music device
         soundMixer = getMixerByName("Logitech Camera");
         if(soundMixer == null) {
             throw new LineUnavailableException();
@@ -58,15 +94,19 @@ public class SoundInput implements OnsetHandler {
 
         JVMAudioInputStream audioStream = new JVMAudioInputStream(stream);
 
+        // The dispatcher is the manager of the device, it routes the data to audio processors
         dispatcher = new BetterAudioDispatcher(audioStream, bufferSize, overlap);
 
         // add a processor, handle percussion event.
-        dispatcher.addAudioProcessor(
+        /*dispatcher.addAudioProcessor(
                 new PercussionDetector(sampleRate,
                         bufferSize,
                         this,
                         sensitivity,
-                        threshold));
+                        threshold));*/
+
+        dispatcher.addAudioProcessor(
+                new PitchProcessor(algo, (float)sampleRate, bufferSize, this));
 
         // run the dispatcher (on a new thread).
         new Thread(dispatcher, "Audio dispatching").start();
@@ -87,7 +127,20 @@ public class SoundInput implements OnsetHandler {
         System.out.println("Percussion at " + v);
     }
 
+    // Callback that handles a pitch detection object
+    @Override
+    public void handlePitch(PitchDetectionResult pitchDetectionResult, AudioEvent audioEvent) {
+        if(pitchDetectionResult.getPitch() != -1) {
+            double timeStamp = audioEvent.getTimeStamp();
+            float pitch = pitchDetectionResult.getPitch();
+
+            Note note = getNoteByFrequency(pitch);
+            System.out.println(note.name());
+        }
+    }
+
     // PRIVATES
+    // Helper method to get a list of mixers
     private List<Mixer.Info> getMixers() {
         List<Mixer.Info> ret = new ArrayList<>();
 
@@ -97,6 +150,7 @@ public class SoundInput implements OnsetHandler {
         return ret;
     }
 
+    // Helper method to get a method by system name
     private Mixer getMixerByName(String name) {
         for(Mixer.Info mi : getMixers()) {
             if(mi.getName().equals(name)) {
@@ -104,5 +158,33 @@ public class SoundInput implements OnsetHandler {
             }
         }
         return null;
+    }
+
+    // Helper method to get a note by frequency, it takes into account
+    // error range and offset
+    private Note getNoteByFrequency(float freq) {
+        float freqF = getFrequencyByNote(Note.F);
+        float freqG = getFrequencyByNote(Note.G);
+        float freqA = getFrequencyByNote(Note.A);
+        float diffFG = freqG - freqF;
+        float diffGA = freqA - freqG;
+        if (freq < freqF - diffFG/2.0f || freq > freqA + diffGA/2.0f) {
+            return Note.None;
+        } else if (freq < freqG - diffFG/2) {
+            return Note.F;
+        } else if (freq < freqA - diffGA/2) {
+            return Note.G;
+        } else {
+            return Note.A;
+        }
+    }
+
+    // Helper method to get the base frequency of a note applying
+    // the note offset if set
+    private float getFrequencyByNote(Note n) {
+        if(baseFreq.containsKey(n)) {
+            return baseFreq.get(n) + noteOffset;
+        }
+        return -1.0f;
     }
 }
